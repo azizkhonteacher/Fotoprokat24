b137ce8220cd0516042d1c26626712d3ce8f0547
azizkhonteacher <azizkhonteacher@gmail.com>
2024-04-23 01:26:13 +0500

    qw

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".product-swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 22,
+
+
+
+//   navigation: {
+//     nextEl: "",
+//     prevEl: "",
+//   },
+});
