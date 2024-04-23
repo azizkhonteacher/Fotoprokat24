0ffa198c4fe623480768597a776988c4daed0016
azizkhonteacher <azizkhonteacher@gmail.com>
2024-04-23 15:04:13 +0500

    s

--- js/app.js
+++ js/app.js
@@ -4,10 +4,42 @@ const swiper = new Swiper(".product-swiper", {
   slidesPerView: 3,
   spaceBetween: 22,
 
-
-
-//   navigation: {
-//     nextEl: "",
-//     prevEl: "",
-//   },
+  //   navigation: {
+  //     nextEl: "",
+  //     prevEl: "",
+  //   },
 });
+
+const productsWrapper = document.getElementById("product-swiper-wrapper");
+
+fetch("https://fakestoreapi.com/products", {
+  method: "GET",
+})
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((product) => {
+      productsWrapper.innerHTML += `
+    <div class="swiper-slide">
+              <div class="products__card">
+                <div class="products__card-img">
+                  <img src="${product.image}" alt="${product.title}">
+                </div>
+                <div class="products__card-title-wrapper">
+                  <a href="#" class="products__card-title">${product.title}</a>
+                 <div class="products__card-title-wrapper__inner">
+                   <span class="products__card-brand">${product.category}</span>
+                   <h4 class="products__card-price">${product.price}$</h4>
+                  </div>
+                </div>
+                <div class="products__card-btns">
+                  <a href="#" class="products__card-more-btn">Подробнее</a>
+                  <button class="products__card-cart-btn">
+                    <img src="./images/svg/cart.svg" alt="products__card-cart-btn">
+                    <span>В корзину</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+    `;
+    });
+  });
